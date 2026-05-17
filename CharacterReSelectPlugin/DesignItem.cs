dfdcf898c5ddf59592e7e1de9ec3cb914944aec9
Arséne <90214968+monlupin@users.noreply.github.com>
2026-05-17 19:12:17 -0400

    refactoring to CharacterReSelect

---
+++ CharacterReSelectPlugin/DesignItem.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace CharacterReSelectPlugin
+{
+    public class DesignItem
+    {
+        public Guid Id { get; }
+        public Guid? ParentFolderId { get; set; }
+        public bool IsFolder { get; }
+        public DesignFolder? Folder { get; }
+        public CharacterDesign? Design { get; }
+        public int SortOrder { get; set; }
+
+        // Folder
+        public DesignItem(DesignFolder f)
+        {
+            Id = f.Id;
+            IsFolder = true;
+            Folder = f;
+            ParentFolderId = f.ParentFolderId;
+            SortOrder = f.SortOrder;
+        }
+
+        // Design
+        public DesignItem(CharacterDesign d)
+        {
+            Id = d.Id;
+            IsFolder = false;
+            Design = d;
+            ParentFolderId = d.FolderId;
+            SortOrder = d.SortOrder;
+        }
+    }
+}
